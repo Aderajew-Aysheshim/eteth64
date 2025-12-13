0ac6e694c60c38e921787d2363f1a8c8519b7467
eteh64 <aysheshimaderajew600@gmail.com>
2025-12-13 12:40:38 +0300

    Remove Desktop and Downloads folders from repo and add .gitignore

--- Desktop/GDG/week3tasks/q4rectangle.py
+++
@@ -1,9 +0,0 @@
-class Rectangle:
-  def __init__(self,width,hight):
-    self.width=width
-    self.hight=hight
-
-  
-rect1=Rectangle(5,10)   
-print("Area of Rectangle:",rect1.width*rect1.hight)
-
